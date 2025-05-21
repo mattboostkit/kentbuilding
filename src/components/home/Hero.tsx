af857982bacd7fc863eaace63c2d6d8746d7730d
mattboostkit <matt@boostkit.io>
2025-05-21 11:21:20 +0100

    Hero Fix

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -52,7 +52,7 @@ const Hero: React.FC = () => {
 
       {/* Content */}
       <div className="container-custom relative h-full flex flex-col justify-center pt-36">
-        <div className="max-w-3xl">
+        <div className="max-w-3xl mt-6 md:mt-0">
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
